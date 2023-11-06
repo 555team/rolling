b4224ed2dc836700b8d48c0789dbfcbd0d64aed3
wecaners <wecaners@naver.com>
2023-11-06 15:50:12 +0900

    refactor: 스타일 컴포넌트 코드 하단 배치, props 구조 분해 사용

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,77 +1,6 @@
 import FriendBadge from 'components/badges/FriendBadge';
 import styled from 'styled-components';
 
-const CardWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-  max-width: 384px;
-  max-height: 280px;
-  padding: 28px 24px;
-  border-radius: 16px;
-  background: ${(props) => props.theme.white};
-  box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
-`;
-
-const ProfileWrapper = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 14px;
-  padding-bottom: 15px;
-  border-bottom: 1px solid ${(props) => props.theme['--gray-200']};
-`;
-
-const ProfileImage = styled.img`
-  width: 56px;
-  height: 56px;
-  border-radius: 100px;
-  border: 1px solid ${(props) => props.theme['--gray-200']};
-`;
-
-const ProfileContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  gap: 6px;
-`;
-
-const ProfileNameWrapper = styled.div`
-  display: flex;
-  gap: 6px;
-`;
-
-const ProfileContentText = styled.span`
-  color: ${(props) => props.theme.black};
-  font-size: 20px;
-  font-style: normal;
-  font-weight: ${(props) => props.weight || 400};
-  line-height: 24px;
-`;
-
-const CardContent = styled.p`
-  color: ${(props) => props.theme['--gray-600']};
-  text-overflow: ellipsis;
-  overflow: hidden;
-  word-break: break-all;
-  font-size: 18px;
-  font-weight: 400;
-  line-height: 26px;
-  letter-spacing: -0.18px;
-  width: 336px;
-  height: 106px;
-  display: -webkit-box;
-  -webkit-line-clamp: 4;
-  -webkit-box-orient: vertical;
-`;
-
-const CardTimeStamp = styled.span`
-  color: ${(props) => props.theme['--gray-400']};
-  font-size: 12px;
-  font-weight: 400;
-  line-height: 18px;
-  letter-spacing: -0.06px;
-`;
-
 function Card() {
   return (
     <CardWrapper>
@@ -98,3 +27,74 @@ function Card() {
 }
 
 export default Card;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+  max-width: 384px;
+  max-height: 280px;
+  padding: 28px 24px;
+  border-radius: 16px;
+  background: ${({ theme }) => theme.white};
+  box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
+`;
+
+const ProfileWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 14px;
+  padding-bottom: 15px;
+  border-bottom: 1px solid ${({ theme }) => theme['--gray-200']};
+`;
+
+const ProfileImage = styled.img`
+  width: 56px;
+  height: 56px;
+  border-radius: 100px;
+  border: 1px solid ${({ theme }) => theme['--gray-200']};
+`;
+
+const ProfileContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 6px;
+`;
+
+const ProfileNameWrapper = styled.div`
+  display: flex;
+  gap: 6px;
+`;
+
+const ProfileContentText = styled.span`
+  color: ${({ theme }) => theme.black};
+  font-size: 20px;
+  font-style: normal;
+  font-weight: ${(props) => props.weight || 400};
+  line-height: 24px;
+`;
+
+const CardContent = styled.p`
+  color: ${({ theme }) => theme['--gray-600']};
+  text-overflow: ellipsis;
+  overflow: hidden;
+  word-break: break-all;
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 26px;
+  letter-spacing: -0.18px;
+  width: 336px;
+  height: 106px;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical;
+`;
+
+const CardTimeStamp = styled.span`
+  color: ${({ theme }) => theme['--gray-400']};
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.06px;
+`;
