86c2e107016a4e3575781306fe4249ad3ca9fc4f
HellenHong <hongjw030@naver.com>
2023-11-09 21:32:44 +0900

    Feat: 토스트 메세지 띄울 곳 지정

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Nav from './nav/Nav';
+import { ToastContainer } from 'react-toastify';
 
 export default function Layout() {
   return (
     <>
       <Nav />
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
