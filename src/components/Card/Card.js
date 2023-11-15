ac0da741d6aa7c2768417ffcae57d051809c40b2
wecaners <wecaners@naver.com>
2023-11-15 23:02:49 +0900

    feat: font 선택 기능 추가

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,6 +3,7 @@ import FamilyBadge from 'components/Badges/FamilyBadge';
 import FriendBadge from 'components/Badges/FriendBadge';
 import OtherBadge from 'components/Badges/OtherBadge';
 import styled from 'styled-components';
+import dompurify from 'dompurify';
 import changeDateFormat from 'utils/calcCreateAt';
 import { ReactComponent as TrashIcon } from '../../assets/icons/trash-icon.svg';
 import { OutlinedButton } from 'components/button/OutlinedButton';
@@ -40,14 +41,25 @@ function Card({
       case 'Pretendard':
         return 'Pretendard, Noto Sans KR';
       case '나눔명조':
-        return 'Nanum Myeongjo, Noto Sans KR';
+        return '나눔명조, Noto Sans KR';
       case '나눔손글씨 손편지체':
-        return 'Handletter, Noto Sans KR';
+        return '나눔손글씨 손편지체, Noto Sans KR';
     }
   };
 
+  const handleContent = (content) => {
+    const sanitizer = dompurify.sanitize;
+    const sanitizedContent = sanitizer(`${content}`);
+    return (
+      <CardContent
+        dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+        fontStyle={handleFontType(font)}
+      ></CardContent>
+    );
+  };
+
   return (
-    <CardWrapper fontStyle={handleFontType(font)}>
+    <CardWrapper>
       <ProfileWrapper>
         <ProfileBox>
           <ProfileImage src={imageUrl} alt="card-profile" />
@@ -69,7 +81,7 @@ function Card({
           </OutlinedButton>
         ) : null}
       </ProfileWrapper>
-      <CardContent>{content}</CardContent>
+      {handleContent(content)}
       <CardTimeStamp>{timeStamp}</CardTimeStamp>
     </CardWrapper>
   );
@@ -92,7 +104,6 @@ const CardWrapper = styled.div`
   border-radius: 16px;
   background: ${({ theme }) => theme.white};
   box-shadow: 0px 2px 12px 0px rgba(0, 0, 0, 0.08);
-  font-family: ${({ fontStyle }) => fontStyle};
   ${({ theme }) => theme.tablet`
     width: 352px;
     height: 284px;
@@ -143,30 +154,44 @@ const ProfileContentText = styled.span`
   `}
 `;
 
-const CardContent = styled.p`
-  color: ${({ theme }) => theme['--gray-600']};
-  text-overflow: ellipsis;
-  overflow: hidden;
-  word-break: break-all;
-  font-size: 18px;
-  font-weight: 400;
-  line-height: 26px;
-  letter-spacing: -0.18px;
-  max-width: 336px;
-  height: 106px;
-  display: -webkit-box;
-  -webkit-line-clamp: 4;
-  -webkit-box-orient: vertical;
-  ${({ theme }) => theme.tablet`
+const CardContent = styled.div`
+  & p {
+    font-family: ${({ fontStyle }) => fontStyle};
+    color: ${({ theme }) => theme['--gray-600']};
+    text-overflow: ellipsis;
+    overflow: hidden;
+    word-break: break-all;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 26px;
+    letter-spacing: -0.18px;
+    max-width: 336px;
+    height: 106px;
+    display: -webkit-box;
+    -webkit-line-clamp: 4;
+    -webkit-box-orient: vertical;
+    ${({ theme }) => theme.tablet`
     width: 304px;
     height: 110px;
   `}
-  ${({ theme }) => theme.mobile`
+    ${({ theme }) => theme.mobile`
     width: 272px;
     height: 56px;
     font-size: 15px;
     line-height: 22px;
   `}
+  }
+  & span {
+    font-family: ${({ fontStyle }) => fontStyle};
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 26px;
+    letter-spacing: -0.18px;
+    ${({ theme }) => theme.mobile`
+    font-size: 15px;
+    line-height: 22px;
+  `}
+  }
 `;
 
 const CardTimeStamp = styled.span`
