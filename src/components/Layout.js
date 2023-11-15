074b4632bf47e81d1fcfb4370770b8785cb58144
taehii <lte1807@naver.com>
2023-11-15 15:58:44 +0900

    header-service commit

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Nav from './Nav/Nav';
 
 export default function Layout() {
