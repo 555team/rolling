49624651494967c16665fea0677b56a43c676586
taehii <lte1807@naver.com>
2023-11-16 15:22:12 +0900

    feat: Post페이지 HeaderService안에 들어갈 EmojiBedge 제작

---
+++ src/components/Badges/EmojiBedge.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { styled } from 'styled-components';
+
+function EmojiBedge({ children }) {
+  return <EmojiBedgeLayout>{children}</EmojiBedgeLayout>;
+}
+
+export default EmojiBedge;
+
+const EmojiBedgeLayout = styled.div`
+  display: flex;
+  height: 30px;
+  padding: 8px 12px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 10px;
+  border-radius: 32px;
+  background: rgba(0, 0, 0, 0.54);
+`;
