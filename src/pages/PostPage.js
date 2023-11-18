74bc1d993fece0d505e1172254f85a0b23b6c519
HellenHong <hongjw030@naver.com>
2023-11-18 20:00:02 +0900

    Feat: 플로팅 버튼 생성

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -14,6 +14,7 @@ import CardModal from 'components/Modal/CardModal';
 import { MainPrimaryButton, SecondaryButton } from 'components/Button/Button';
 import SkeletonCard from 'components/Skeleton/SkeletonCard';
 import newMessage from '../assets/img/new-message.gif';
+import { FloatingButton } from 'components/Button/FloatingButton';
 
 function PostPage() {
   const { id } = useParams();
@@ -194,6 +195,7 @@ function PostPage() {
           : null}
         <Target ref={target} />
       </CardListWrapper>
+      <FloatingButton />
     </PostPageWrapper>
   );
 }
